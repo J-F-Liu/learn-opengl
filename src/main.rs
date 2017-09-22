8bff4a25d695744f48da748264f9fb6fda503afa
Junfeng <china.liujunfeng@gmail.com>
2017-09-22 10:43:47 +0800

    Colorful triangle

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ use glium::Surface;
 #[derive(Copy, Clone)]
 struct Vertex {
     position: [f32; 2],
+    color: [f32; 3],
 }
 
-implement_vertex!(Vertex, position);
+implement_vertex!(Vertex, position, color);
 
 
 fn main() {
@@ -19,9 +20,9 @@ fn main() {
     let context = glium::glutin::ContextBuilder::new();
     let display = glium::Display::new(window, context, &events_loop).unwrap();
 
-    let vertex1 = Vertex { position: [-0.5, -0.5] };
-    let vertex2 = Vertex { position: [ 0.0,  0.5] };
-    let vertex3 = Vertex { position: [ 0.5, -0.25] };
+    let vertex1 = Vertex { position: [-0.5, -0.5], color: [1.0, 0.0, 0.0] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5], color: [0.0, 1.0, 0.0] };
+    let vertex3 = Vertex { position: [ 0.5, -0.5], color: [0.0, 0.0, 1.0] };
     let shape = vec![vertex1, vertex2, vertex3];
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
@@ -31,19 +32,22 @@ fn main() {
         #version 140
 
         in vec2 position;
+        in vec3 color;
+        out vec3 mid_color;
 
         void main() {
             gl_Position = vec4(position, 0.0, 1.0);
+            mid_color = color;
         }
     "#;
 
     let fragment_shader_src = r#"
         #version 140
-
+        in vec3 mid_color;
         out vec4 color;
 
         void main() {
-            color = vec4(1.0, 0.5, 0.2, 1.0);
+            color = vec4(mid_color, 1.0);
         }
     "#;
 
